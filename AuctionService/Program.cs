43effc3d8483db7fd07bf29d8584c8d782d77746
ChernoCham <cherno@writemedia.co.uk>
2024-03-31 03:49:13 +0000

    end of section 3

---
+++ AuctionService/Program.cs
@@ -0,0 +1,33 @@
+using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AuctionDbContext>(options =>
+{
+    var connectionString = $"Server=localhost;Port=5432;Username=postgres;Password=password123;Database=auction;";
+    options.UseNpgsql(connectionString);
+});
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+try
+{
+    DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
+
+app.Run();
